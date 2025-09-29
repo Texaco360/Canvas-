81d66ee5b7a2ccd3a64b044ddfedce36ecad258b
Texaco360 <decrooskoenraad@gmail.com>
2025-09-29 20:36:40 +0200

    working ruler and lines

--- .vscode/CompileOmniPascalServerProject.sh
+++ .vscode/CompileOmniPascalServerProject.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 LAZBUILD="/usr/bin/lazbuild"
-PROJECT="/mnt/c/Users/decro/Code/Objectpascal/Test Canvas/project1.lpi"
+PROJECT="/home/koenraad/Code/Canvas/project1.lpi"
 
 # Modify .lpr file in order to avoid nothing-to-do-bug (http://lists.lazarus.freepascal.org/pipermail/lazarus/2016-February/097554.html)
-echo >> "$PROJECT"
+echo. >> "/home/koenraad/Code/Canvas/project1.lpr"
 
-if "$LAZBUILD" "$PROJECT"; then
+if $LAZBUILD --build-mode=Debug $PROJECT; then
 
-  if [ "$1" = "test" ]; then
-    "/mnt/c/Users/decro/Code/Objectpascal/Test Canvas/project1" 
+  if [ $1 = "test" ]; then
+    "/home/koenraad/Code/Canvas/project1" 
   fi
 fi
--- unit1.pas
+++ unit1.pas
@@ -16,16 +16,29 @@ type
   TDrawingForm = class(TForm)
     PaintBox: TPaintBox;
     procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
     procedure PaintBoxPaint(Sender: TObject);
-
+    procedure PaintBoxMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
     private
+      RulerBitmap: TBGRABitmap;
+      procedure DrawLines(Ctx: TBGRACanvas2D);
       procedure DrawRuler( Ctx: TBGRACanvas2D; const AWidth, AOrigin, AScale: Integer );
+      procedure UpdateRulerBitmap;
     // procedure DrawHorizontalRuler(Ctx: TBGRACanvas2D; const R: TRect);
+    
   end;
 
   type TPoint = record
     x: Double;
     y: Double;
+  end;
+
+  type TLine = record
+    startPoint: TPoint;
+    endPoint: TPoint;
   end;
 
 var
@@ -37,14 +50,50 @@ implementation
 
 
 const
-  cRulerHeight = 30;
+  cRulerHeight = 25;
 var
-  scale : Integer; //1mm = 10 du
+  scale : Integer; //1mm = 5 du
+  isDrawing : Boolean = False;
+  startPoint, endPoint : TPoint;
+  lineList : array of TLine;
+  currentLine : TLine;
+  lineCount : Integer = 0;
+
+procedure TDrawingForm.UpdateRulerBitmap;
+var
+  ctx: TBGRACanvas2D;
+  R: TRect;
+  FWidth: Integer;
+  FOrigin: TPoint;
+  begin
+    if Assigned(RulerBitmap) then RulerBitmap.Free;
+    RulerBitmap := TBGRABitmap.Create(ClientWidth, ClientHeight, BGRAWhite);
+    ctx := RulerBitmap.Canvas2D;
+
+    // Draw horizontal ruler
+    R := Rect(0, 0, RulerBitmap.Width-30, RulerBitmap.Height-30);
+    FOrigin.x := RulerBitmap.Width/2;
+    FOrigin.y := RulerBitmap.Height/2;
+    FWidth := R.Width;
+    ctx.save();
+    ctx.translate(45,0);
+    DrawRuler(ctx, FWidth, Floor(FOrigin.x), scale );
+    ctx.restore;
+
+    // Draw vertical ruler
+    FWidth := R.Height;
+    ctx.save();
+    ctx.translate(0,RulerBitmap.Height);
+    ctx.rotate(-pi/2);
+    DrawRuler(ctx, FWidth, Floor(FOrigin.y),scale);
+    ctx.restore;
+end;
+
 
 procedure TDrawingForm.DrawRuler(Ctx: TBGRACanvas2D; const AWidth, AOrigin, AScale: Integer);
 const
-  MinorTick   = 10;   // every 10 px a short tick
-  MajorTick   = 50;   // every 50 px a longer tick + number
+  MinorTick   = 8;   // every 10 px a short tick
+  MajorTick   = 40;   // every 50 px a longer tick + number
 var
   x, y0, yMinor, yMajor: Integer;
   txt: string;
@@ -63,7 +112,7 @@ begin
 
   // ---- 3️⃣ Common style -----------------------------------------
   Ctx.lineWidth   := 1;
-  Ctx.font        := '20px Tahoma';
+  Ctx.font        := '14px Tahoma';
   Ctx.textBaseline:= 'top';           // numbers will be placed below the tick
   Ctx.textAlign   := 'center';
 
@@ -103,63 +152,101 @@ begin
   end;
 end;
 
-
 procedure TDrawingForm.FormCreate(Sender: TObject);
 begin
      PaintBox.Align := alClient;
+     scale := 2; //1mm = 10du
+     UpdateRulerBitmap;
     PaintBox.Repaint;
+end;
+
+procedure TDrawingForm.FormResize(Sender: TObject);
+begin
+  UpdateRulerBitmap;
+  PaintBox.Repaint;
+end;
+
+procedure TDrawingForm.PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+begin
+//start drawing a line
+  if not isDrawing then
+  begin
+    isDrawing := True;
+    startPoint.x := X;
+    startPoint.y := Y;
+    currentLine.startPoint := startPoint;
+    currentLine.endPoint := startPoint;
+  end
+  else
+  begin
+    isDrawing := False;
+    endPoint.x := X;
+    endPoint.y := Y;
+    currentLine.endPoint := endPoint;
+    SetLength(lineList, lineCount + 1);
+    lineList[lineCount] := currentLine;
+    Inc(lineCount);
+    PaintBox.Repaint;
+  end;
+end;
+
+procedure TDrawingForm.PaintBoxMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+begin
+//update the current line end point
+  if isDrawing then
+  begin
+    endPoint.x := X;
+    endPoint.y := Y;
+    currentLine.endPoint := endPoint;
+    PaintBox.Repaint;
+  end;
+end;
+
+procedure TDrawingForm.DrawLines(Ctx: TBGRACanvas2D);
+var i: integer;
+begin
+  Ctx.strokeStyle('blue');
+  Ctx.lineWidth := 2;
+  Ctx.beginPath;
+  for i := 0 to High(lineList) do
+  begin
+    Ctx.moveTo(lineList[i].startPoint.x, lineList[i].startPoint.y);
+    Ctx.lineTo(lineList[i].endPoint.x, lineList[i].endPoint.y);
+  end;
+  Ctx.stroke;
+
+  //draw the current line in red
+  if isDrawing then
+  begin
+    Ctx.strokeStyle('red');
+    Ctx.lineWidth := 2;
+    Ctx.beginPath;
+    Ctx.moveTo(currentLine.startPoint.x, currentLine.startPoint.y);
+    Ctx.lineTo(currentLine.endPoint.x, currentLine.endPoint.y);
+    Ctx.stroke;
+  end;
 end;
 
 procedure TDrawingForm.PaintBoxPaint(Sender: TObject);
 var
   bmp : TBGRABitmap;
   ctx : TBGRACanvas2D;
-  R   : TRect;
-  FWidth : Integer;
-  FOrigin : TPoint;
 begin
   // --------------------------------------------------------------
   // 1️⃣ Create a bitmap the size of the client area
   // --------------------------------------------------------------
   bmp := TBGRABitmap.Create(ClientWidth, ClientHeight, BGRAWhite);
-  scale := 5; //1mm = 10du
-  try
+  
+  try  
     ctx := bmp.Canvas2D;
+    if Assigned(RulerBitmap) then
+      bmp.PutImage(0, 0, RulerBitmap, dmDrawWithTransparency);
 
-    // ctx.scale(3,3);
-    // --------------------------------------------------------------
-    // 2️⃣ Draw the horizontal ruler at the very top
-    // --------------------------------------------------------------
-    R := Rect(0, 0, bmp.Width-30, bmp.Height-30);
-    FOrigin.x := bmp.Width/2;
-    FOrigin.y := bmp.Height/2;
-    FWidth := R.Width;
-    ctx.save();
-    ctx.translate(45,0);
-    DrawRuler(ctx, FWidth, Floor(FOrigin.x), scale );
-     ctx.restore;
-
-    FWidth := R.Height;
-    ctx.save();
-    ctx.translate(0,bmp.Height);
-    ctx.rotate(-pi/2);
-    DrawRuler(ctx, FWidth, Floor(FOrigin.y),scale);
-    ctx.restore;
-
-    ctx.translate(45,0);
+    DrawLines(ctx);
 
 
-
-    // --------------------------------------------------------------
-    // 3️⃣ Your original demo graphics (orange rectangle)
-    // --------------------------------------------------------------
-    ctx.fillStyle('rgb(240,128,5)');
-    ctx.fillRect(30, 30 + cRulerHeight, 80, 60);   // shift down by ruler height
-    ctx.strokeRect(50, 50 + cRulerHeight, 80, 60);
-
-    // --------------------------------------------------------------
-    // 4️⃣ Blit the bitmap onto the PaintBox canvas
-    // --------------------------------------------------------------
     bmp.Draw(PaintBox.Canvas, 0, 0);
   finally
     bmp.Free;
